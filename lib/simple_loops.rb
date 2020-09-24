c862471ba11549bed2f3339904339d9566b71ed4
akam alam <arundelfootball23@gmail.com>
2020-09-24 02:18:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,5 +26,11 @@ def output_array(array)
 end 
 
 def return_string_array(array)
-  
+  count = 0 
+  new_array = []
+  while count < array.length do 
+    new_array.push(array[count].to_s)
+    array[count].to_s 
+    count += 1 
+    
 end 
